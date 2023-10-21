3c82aea533e63efa073b7d3c20a841f5c3bfb1a9
bini1214 <122049945+bini1214@users.noreply.github.com>
2023-10-21 22:49:49 +0300

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,17 +1,18 @@
-class Solution:
+lass Solution:
     def reverse(self, x: int) -> int:
-
-        reversed = 0 
-        sign = 1
-
-        if not  -2147483648 < x < 2147483647 :
-            return 0
-
-        if x < 0 :
-            sign = -1 
-            x *= sign 
-
-        while (x > 0) :
-            a = x % 10
-            reversed = reversed * 10 + a
-            x = x // 10
+        if x >= 0:
+            xstr = str(x)
+            y = xstr[::-1]
+            x = int(y)
+            if -2**31 <= x < 2**31:
+                return x
+            else:
+                return 0
+        else:
+            xstr = str(-x)
+            y = xstr[::-1]
+            x = int(y)
+            if -2**31 <= x < 2**31:
+                return -x
+            else:
+                return 0  
